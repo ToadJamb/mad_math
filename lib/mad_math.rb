2ab7d077412d80a15406a71056075bb27779ff3a
Travis Herrick <tthetoad@gmail.com>
2017-01-29 22:46:08 -0800

    do not include NewtonPolynomial by default

--- lib/mad_math.rb
+++ lib/mad_math.rb
@@ -1,2 +1,1 @@
 require_relative 'mad_math/stats'
-require_relative 'mad_math/newton_polynomial'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative '../lib/mad_math'
+require_relative '../lib/mad_math/all'
 
 base = File.expand_path(File.dirname(__FILE__))
 path = File.join(base, 'support')
